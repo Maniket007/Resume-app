eae795133d2e3e8b83a781bd4defae2571ff7e54
Maniket Kumar <sonuanand644@gmail.com>
2022-06-27 11:00:30 +0530

    updated

---
+++ resume-app/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Homepage from "./Components/Homepage/Homepage";
+//Portfolo website..
+
+//1. NavBar..
+//2. HomePage..
+//3. Tech Stack..
+//4. Experience..
+//5. Projects..
+
+
+
+function App() {
+  return (
+    <div className="app">
+      <Navbar />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ resume-app/src/Components/Homepage/Homepage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./Homepage.css";
+import github from "../../Images/Github.png";
+import linkedin from "../../Images/linkedin.png";
+
+function HomePage(){
+    return (
+        <div className="Homepage_container">
+            <div className="Homepage_left">
+                <p className="Homepage_left_h1">
+                    <span className="red_color">Hi, </span>My name is
+                </p>
+                <p className="Homepage_left_h2">Maniket Kumar</p>
+                <p className="Homepage_left_h3">I am a Software developer at <span className="red_color">Cognizant.</span>{" "}
+                </p>
+                <p className="Homepage_left_description">I love to develop website.{" "}</p>
+                <p className="Homepage_left_description">Have a look at my <span className="red_color">Portfolio.</span>{" "}</p>
+                <p className="Homepage_left_description">I am also a <span className="red_color">Competetitive coder.</span>{" "}
+                </p>
+                <div>
+                    <a href="https://github.com/Maniket007" target="_blank">
+                        <img src={github} alt="githubImage"/>
+                    </a>
+                    <a href="https://www.linkedin.com/in/maniket-kumar-55360216b/" target="_blank">
+                        <img src={linkedin} alt="linkedinImage"/>
+                    </a>
+                </div>
+                <a href="sonuanand644@gmail.com">
+                    <button className="homepage_left_button">Get in Touch</button>
+                </a>
+            </div>
+        </div>
+    );
+}
+export default HomePage;
